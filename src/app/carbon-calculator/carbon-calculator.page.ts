142b5fb21936ad98d2c9767da5f2287eef7af58f
Anya <nagavardhanraju@gmail.com>
2022-12-04 02:27:19 +0530

    Completed

--- src/app/carbon-calculator/carbon-calculator.page.ts
+++ src/app/carbon-calculator/carbon-calculator.page.ts
@@ -12,6 +12,7 @@ export class CarbonCalculatorPage implements OnInit {
 
   public settingsObj: Settings;
   public currentDiv = 0;
+  public score = 0;
 
 
   constructor(private settings: SettingsService) {
@@ -24,12 +25,21 @@ export class CarbonCalculatorPage implements OnInit {
   }
 
   public nextDiv(nextScr) {
-    //this.currentDiv = nextScr;
-    this.currentDiv = this.currentDiv + nextScr;
-    this.scrollToTop();
+    this.currentDiv = this.currentDiv + 1;
+    this.score = (this.score) + (nextScr);
+
+
   }
   scrollToTop() {
     //this.content.scrollToTop(400);
+  }
+
+  one() {
+    alert(1);
+  }
+
+  two() {
+    alert(2);
   }
 
 
